e8b0afd234c00b59b2dd8f2c839b5488b060920d
Julien Cherry <juliencherry@gmail.com>
2018-01-27 19:55:37 -0500

    Add note + voicemail example

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,7 @@ use cursive::traits::*;
 use cursive::align::*;
 use cursive::view::Boxable;
 use cursive::view::SizeConstraint;
-use cursive::views::{Dialog, Panel, TextView, TextArea};
-
+use cursive::views::{Button, Dialog, DummyView, EditView, LinearLayout, ListView, TextArea, TextView};
 
 fn main() {
     // let mut snd = Sound::new("src/test.ogg").unwrap();
@@ -26,11 +25,28 @@ fn main() {
     // The text is too long to fit on a line, so the view will wrap lines,
     // and will adapt to the terminal size.
 
+    let text = "This is a very simple example of linear layout. Two views \
+                are present, a short title above, and this text. The text \
+                has a fixed width, and the title is centered horizontally.";
+
     siv.add_layer(
-        Dialog::new()
-            .title("Notes")
-            .content(TextArea::new())
-            .fixed_size((80, 24))
+        Dialog::around(
+            LinearLayout::horizontal()
+            .child(Dialog::new()
+                .title("Notes")
+                .content(TextArea::new())
+                .fixed_size((60, 24))
+            )
+            .child(Dialog::new()
+                .title("Voicemail")
+                .content(
+                    ListView::new()
+                        .child("G’ma • 06/12/07", Button::new("Play", |x|0))
+                        .child("Joyce • 06/13/07 6:00 p.m.", Button::new("Play", |x|0))
+                        .fixed_size((40, 24))
+                )
+            )
+        )
     );
 
 
